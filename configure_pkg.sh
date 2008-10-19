d12a74d6eb55a9390c6dcfef5ffe57f2668e4a1c
Lee Thompson <bm55b@yahoo.com>
2008-10-19 01:33:14 -0500

    deal with GMP library on Mac

--- configure_pkg.sh
+++ configure_pkg.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 # Clean out the package of all machine made files
-rm -rf autom4te.cache aclocal.m4 config cpanplus DESTDIR get_cpan_STDIN.txt
+rm -rf autom4te.cache aclocal.m4 config cpanplus DESTDIR get_cpan_STDIN.txt \
+  Makefile Makefile.in configure *.pl
+
 # Now recreate those machine made files and collect the CPAN tarballs
 autoreconf -i
+
+
 perl_path=$(which perl | sed -e 's,/bin/perl,,')
 echo "Using perl in path ${perl_path}"
-./configure --prefix=${perl_path} --with-perl=${perl_path}
+machine=$(uname -s)
+# gmp is included with most linux distros, but not on MacIntosh
+if [[ "${machine}" = "Darwin" ]]; then
+  echo "Using machine ${machine}"
+  ./configure --prefix=${perl_path} --with-gmp=${perl_path} \
+    --with-perl=${perl_path} || exit 1;
+else
+  ./configure --prefix=${perl_path} --with-perl=${perl_path} || exit 1;
+fi
 # package generated and man made stuff into a tarball
 make dist
