a47a026fadbf57885b91cf7a49969be51e12aa0f
Mike Espe <mikeespe@yahoo.com>
2008-10-21 15:31:33 -0400

    Making version 0.4.3 to remove the bad Net::SFTP module and replace with Net::SSH instead

--- configure_pkg.sh
+++ configure_pkg.sh
@@ -6,17 +6,8 @@ rm -rf autom4te.cache aclocal.m4 config cpanplus DESTDIR get_cpan_STDIN.txt \
 # Now recreate those machine made files and collect the CPAN tarballs
 autoreconf -i
 
-
 perl_path=$(which perl | sed -e 's,/bin/perl,,')
 echo "Using perl in path ${perl_path}"
-machine=$(uname -s)
-# gmp is included with most linux distros, but not on MacIntosh
-if [[ "${machine}" = "Darwin" ]]; then
-  echo "Using machine ${machine}"
-  ./configure --prefix=${perl_path} --with-gmp=${perl_path} \
-    --with-perl=${perl_path} || exit 1;
-else
-  ./configure --prefix=${perl_path} --with-perl=${perl_path} || exit 1;
-fi
+./configure --prefix=${perl_path} --with-perl=${perl_path} || exit 1;
 # package generated and man made stuff into a tarball
 make dist
